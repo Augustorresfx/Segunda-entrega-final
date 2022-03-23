e2a6e7028c959ddcdf391339e4d5589b928d65c9
Augustorresfx <augustorresfx@gmail.com>
2022-03-23 20:44:28 -0300

    entrega

---
+++ src/config.js
@@ -0,0 +1,26 @@
+export default {
+    fileSystem: {
+      path: "../DB",
+    },
+    mongodb: {
+      cnxStr: "mongodb://127.0.0.1:27017/ecommerce",
+      options: {
+        serverSelectionTimeoutMS: 5000,
+      },
+    },
+    firebase: {
+      type: "service_account",
+      project_id: "proyecto-backend-a15df",
+      private_key_id: "75822231c5e1f2aa67c5fdccd70ed6a02400f811",
+      private_key:
+      "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCpR+GcrYOjUNgZ\nd/iN1B3UOtnutVZn+64aMHtWrznXLcUAu8xvfY9avFg1BuPijRhQopbYh7NfmrhM\nAaZ3RFFlnJchYXBFW5pUnzzk4nDM0fhU7f781deWWcTcF3DiJFyc2i7pnDEnCDiQ\n0HLKszzZpkn/NWxjBQWG+8nGNoadm/F/1ETP+0IrS3PE90KRmGHZwjroPQQKm57z\n/28wshovzc+GCV16mpRWJZGerY6X3yQmH1F25KnRNqm25gR6GZMP3BzC/iDmDGSR\n3J0WYsxgwbeu3KGGnfKszfQ3Qvs0UdznSdN+2l2/UIOlyYWqvuqkr5WcHbUQ3vqO\nuNxC3crLAgMBAAECggEAS4EfzQIJzpz/HnEIAL7FZUWwsgvZE/1KvSy1vsbefi9Q\niRC60VnISLjsgQ5nM8QqPcEflkoExFZe8Kvlw5nJ0pBDSqTK7my6zBbQkui7Vmgr\nIzqaesyo4ct9hovAEmYUlvOsbA96TwYYMOOA6A+FSzEkHTyHcS0qt3f0GkB0z7Ql\nn9d4aI4MVED+Id1APE8SiodGZutL2H6gnV0QFIk4wOLb7ASD7rul/+Hq1qS+RN/z\nZm8OJU26HEdBspxQl9sD6QY0uvm816nGiRX6YEeY6pNpu8s/qvrtYoSzi6eiTgDt\nPS6CZHLEDAf7f7O3ynlgkF0SmvtJsp75T02e/R7tjQKBgQDmPsEDiEEpU8jXwJy2\n7kaNithxZkEDJzivaIEnukEi2i0OqcU7nBJNKtcuUx8xv16ugCwlI3WoSZLQiVOq\n9xObMvdzgLNG2ntFGqERtyp/UcahK2yyhAMJyoj+yLDqGttt7x1VrNB/ScQYf57v\nDL0tBSvSyJZGuuiPsGHxQO48vQKBgQC8N13TDS2JkEkbh69Qg64KS0fIrI/+I+DT\ni/OVlsobpbGqhmkrQD2zMeAMSGBYovkTnacC5iJyduNEa9oOQ62XmSATTLf7lEXu\noTm8fdLoRPA3iotT9lfyAEbby4pdYcpNuW5/WDxD23l2WTnhGpwKuB0ibdF9HFyg\n/03UUHZSJwKBgQCVWLM+JU2PDwfB2vBMQDaHbT6EH/2fm/tK3BCawlep0bdUQY7r\n4YqFf3pgVA5x6F3z9rOry1pwNHkEx278AJQ5IVAdHagsF1/KLUG5CgSI3XRn6pDx\nImwOF8/Q5EDNOWqH3og6oIQFbMXD1lQzXyH+Z9JLQU0ox+g9+/0rziA6SQKBgEk6\nWVy1sdPluSvm3IS81sfWhjVDPjqoht6HgtvecP/UtY0dAmn7coX3j633stSJg9Ln\n8YSlOR3Dt6R1Ob93AWEMCHi5zpMpGxQ+0hHN+nK376YMVyzxmUHkba0yQcEXPPiE\nfsM49ymHiHhNOsGj8rYQ7/TnOSwuDrPs4MEMFRzdAoGAEqriV2AuweYGGH9mfAOk\nTMSlw56XfLYoymM3PSM6/DSGI4TPRfEoCKwegqrismONA5eEuJCQjaB5XRJzWvA3\nh4kKynD6V8p8/M5GI0PYMW4XNCYjuE1LEd8D77sYHp1h9lqAPpvFLUwYfWI3ablY\nLHLG8kBJSA3Jr4VQsNFZMdk=\n-----END PRIVATE KEY-----\n",
+      client_email:
+        "firebase-adminsdk-ch632@proyecto-backend-a15df.iam.gserviceaccount.com",
+      client_id: "109986771403354343551",
+      auth_uri: "https://accounts.google.com/o/oauth2/auth",
+      token_uri: "https://oauth2.googleapis.com/token",
+      auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+      client_x509_cert_url:
+        "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ch632%40proyecto-backend-a15df.iam.gserviceaccount.com",
+    },
+  };
